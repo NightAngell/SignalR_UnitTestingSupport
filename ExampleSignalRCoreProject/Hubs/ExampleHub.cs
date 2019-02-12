5c5dbd53556a831b36d21f63811adbee3bf74dfe
NightAngell <sobta24@wp.pl>
2019-02-12 13:48:39 +0100

    Added small fix and tests for generic hub

--- ExampleSignalRCoreProject/Hubs/ExampleHub.cs
+++ ExampleSignalRCoreProject/Hubs/ExampleHub.cs
@@ -36,5 +36,78 @@ namespace ExampleSignalRCoreProject.Hubs
 
             await _db.SaveChangesAsync();
         }
+
+        public void NotifyAboutSomethingElseAllExcept()
+        {
+            var excludedConnectionIds = new List<string> {};
+            Clients.AllExcept(excludedConnectionIds.AsReadOnly()).NotifyAboutSomethingElse();
+        }
+
+        public void NotifyCallerAboutSomethingElse()
+        {
+            Clients.Caller.NotifyAboutSomethingElse();
+        }
+
+        public void NotifyClientAboutSomethingElse()
+        {
+            Clients.Client("").NotifyAboutSomethingElse();
+        }
+
+        public void NotifyClientsAboutSomethingElse()
+        {
+            var excludedConnectionIds = new List<string> { };
+            Clients.Clients(excludedConnectionIds).NotifyAboutSomethingElse();
+        }
+
+        public void NotifyGroupAboutSomethingElse()
+        {
+            Clients.Group("").NotifyAboutSomethingElse();
+        }
+
+        public void NotifyGroupExceptAboutSomethingElse()
+        {
+            Clients.GroupExcept("", new List<string>().AsReadOnly()).NotifyAboutSomethingElse();
+        }
+
+        public void NotifyGroupsAboutSomethingElse()
+        {
+            Clients.Groups(new List<string>().AsReadOnly()).NotifyAboutSomethingElse();
+        }
+
+        public void NotifyOthersAboutSomethingElse()
+        {
+            Clients.Others.NotifyAboutSomethingElse();
+        }
+
+        public void NotifyOthersInGroupAboutSomethingElse()
+        {
+            Clients.OthersInGroup("").NotifyAboutSomethingElse();
+        }
+
+        public void NotifyUserAboutSomethingElse()
+        {
+            Clients.User("").NotifyAboutSomethingElse();
+        }
+
+        public void NotifyUsersAboutSomethingElse()
+        {
+            Clients.Users(new List<string>().AsReadOnly()).NotifyAboutSomethingElse();
+        }
+
+        public void AddLoremAsKeyAndIpsumAsValueToContextItems()
+        {
+            Context.Items.Add("Lorem", "Ipsum");
+        }
+
+        public void AddSomebodyToGroup()
+        {
+            Groups.AddToGroupAsync("", "");
+        }
+
+        public void RemoveSomebodyFromGroup()
+        {
+            Groups.RemoveFromGroupAsync("", "");
+        }
+
     }
 }
