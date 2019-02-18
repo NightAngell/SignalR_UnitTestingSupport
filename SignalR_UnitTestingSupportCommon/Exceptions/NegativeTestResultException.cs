f39cb8b7f3b2d9c5b6742f74adc91f0889bf10f6
NightAngell <sobta24@wp.pl>
2019-02-18 12:05:47 +0100

    Created common common base class

---
+++ SignalR_UnitTestingSupportCommon/Exceptions/NegativeTestResultException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR_UnitTestingSupportCommon.Exceptions
+{
+    public class NegativeTestResultException : Exception
+    {
+        public NegativeTestResultException() : base() { }
+      
+        public NegativeTestResultException(string message) : base(message) { }
+       
+        public NegativeTestResultException(string message, Exception innerException)
+        : base (message, innerException) { }
+    }
+}
