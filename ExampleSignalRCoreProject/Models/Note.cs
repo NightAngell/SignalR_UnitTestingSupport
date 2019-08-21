3951cc18b1d8d060f73c63587a2fc3b39595c502
NightAngell <sobta24@wp.pl>
2019-08-21 20:28:11 +0200

    Removed unnecessary using statements from models.

--- ExampleSignalRCoreProject/Models/Note.cs
+++ ExampleSignalRCoreProject/Models/Note.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ExampleSignalRCoreProject.Models
+﻿namespace ExampleSignalRCoreProject.Models
 {
     public class Note
     {
