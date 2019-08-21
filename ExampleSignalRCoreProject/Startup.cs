ba42363c2a4ddd46c3a06f6b06824f45d240f704
NightAngell <sobta24@wp.pl>
2019-08-21 20:21:18 +0200

    Removed unnecessary using statements in Startup.cs

--- ExampleSignalRCoreProject/Startup.cs
+++ ExampleSignalRCoreProject/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ExampleSignalRCoreProject.Databases;
+﻿using ExampleSignalRCoreProject.Databases;
 using ExampleSignalRCoreProject.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,8 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ExampleSignalRCoreProject
 {
