21f15a957a4da93346591f7e61a58929eab29354
NightAngell <sobta24@wp.pl>
2019-08-21 20:23:37 +0200

    Removed unnecessary using statements form Db.cs

--- ExampleSignalRCoreProject/Databases/Db.cs
+++ ExampleSignalRCoreProject/Databases/Db.cs
@@ -1,9 +1,5 @@
 ﻿using ExampleSignalRCoreProject.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExampleSignalRCoreProject.Databases
 {
