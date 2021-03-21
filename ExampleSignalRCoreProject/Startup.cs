05efe21d81e95add80a50dffd94f00a481a43553
Mateusz Soboń <sobta24@wp.pl>
2021-03-21 16:40:53 +0100

    Removed unused usings from Startup file

--- ExampleSignalRCoreProject/Startup.cs
+++ ExampleSignalRCoreProject/Startup.cs
@@ -2,14 +2,9 @@ using ExampleSignalRCoreProject.Databases;
 using ExampleSignalRCoreProject.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExampleSignalRCoreProject
 {
